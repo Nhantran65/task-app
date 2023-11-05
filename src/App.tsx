0603d3764bef2825c40e655356703003722c0b13
Nhân Trần Huỳnh Đại <rim@dsl-trebng12-50dc77-3.dhcp.inet.fi>
2023-11-05 23:15:41 +0200

    Nhan add

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import About from './components/About'; // Import your About component
 import Navbar from './layout/NavBar'; // Import your Navbar component
 import TaskList from './components/TaskList';
 import TaskTable from './components/TaskTable';
-
+import Image from './components/Image';
 function App() {
   return (
     <Router>
@@ -13,6 +13,7 @@ function App() {
       <Route path="/" element={<TaskList />} />
       <Route path="/about" element={<About />} />
       <Route path="/table" element={<TaskTable />} />
+      <Route path="/image" element={<Image />} />
       {/* Add more routes for other pages as needed */}
     </Routes>
   </Router>
